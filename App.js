3dcd9bcf1c5bb87695e2e273f9f17c24d78a2149
eliezer <eliezerbuczak@gmail.com>
2024-02-27 22:08:40 -0300

    fisrt commi

--- App.js
+++ App.js
@@ -5,13 +5,15 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
+    flexDirection: 'column',
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',

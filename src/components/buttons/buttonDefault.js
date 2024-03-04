da90b8e3126871044baa41c72390d28f45f8eaa1
eliezer <eliezerbuczak@gmail.com>
2024-03-03 23:18:27 -0300

    style components

--- src/components/buttons/buttonDefault.js
+++ src/components/buttons/buttonDefault.js
@@ -1,29 +1,11 @@
 import React from 'react';
-import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity, Text , View} from 'react-native';
 
 export default function ButtonDefault({ text, onPress }) {
     return (
-        <TouchableOpacity style={styles.button} onPress={onPress}>
-            <Text style={styles.buttonText}>{text}</Text>
+        <TouchableOpacity className="bg-gray-800 p-4 w-10/12 rounded-lg" onPress={onPress}>
+            <Text className="text-white font-bold text-center">{text}</Text>
         </TouchableOpacity>
     );
 }
 
-const styles = StyleSheet.create({
-    button: {
-        backgroundColor: '#4CAF50', // Cor de fundo verde
-        borderRadius: 5,
-        padding: 10,
-        alignItems: 'center',
-        justifyContent: 'center',
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.5,
-        shadowRadius: 2,
-        elevation: 3, // Elevação para adicionar sombra no Android
-    },
-    buttonText: {
-        color: 'white',
-        fontSize: 16,
-    },
-});
